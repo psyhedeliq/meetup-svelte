d66d049697ae8e86c5f9f014be3009fb39539470
Cornel <filipcornelcristian@gmail.com>
2020-04-19 22:45:16 +0300

    create and style Button component

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,7 @@
   import Header from "./UI/Header.svelte";
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
+  import Button from "./UI/Button.svelte";
 
   let title = "";
   let subtitle = "";
@@ -101,7 +102,7 @@
       controlType="textarea"
       value={description}
       on:input={event => (description = event.target.value)} />
-    <button type="submit">Save</button>
+    <Button type="submit" caption="Save" />
   </form>
   <MeetupGrid {meetups} />
 </main>
