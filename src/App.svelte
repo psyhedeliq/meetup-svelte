4732468dc85ddc861a34f0f35adedeb09932db9d
Cornel <filipcornelcristian@gmail.com>
2020-04-23 18:05:47 +0300

    create EditMeetup component (move the form into it's own component)

--- src/App.svelte
+++ src/App.svelte
@@ -3,23 +3,16 @@
   import MeetupGrid from "./Meetups/MeetupGrid.svelte";
   import TextInput from "./UI/TextInput.svelte";
   import Button from "./UI/Button.svelte";
-
-  let title = "";
-  let subtitle = "";
-  let address = "";
-  let email = "";
-  let description = "";
-  let imageUrl = "";
+  import EditMeetup from "./Meetups/EditMeetup.svelte";
 
   let meetups = [
     {
       id: "m1",
       title: "Coding Bootcamp",
       subtitle: "Learn to code in 8 weeks",
-      description:
-        "In this meetup we will have some experts that teach you how to code!",
+      description: "In this Bootcamp you will write your first line of code!",
       imageUrl:
-        "https://cdn.pixabay.com/photo/2016/09/08/04/12/programmer-1653351_1280.png",
+        "https://cdn.pixabay.com/photo/2016/11/18/18/37/programming-1836330_1280.png",
       address: "5th Avenue Road, 323454 Los Angeles",
       contactEmail: "code@test.com",
       isFavorite: false
@@ -30,26 +23,30 @@
       subtitle: "Get your black belt",
       description: "In this karate course you will learn to defend yourself!",
       imageUrl:
-        "https://cdn.pixabay.com/photo/2017/11/21/12/45/karate-2968106_1280.jpg",
+        "https://cdn.pixabay.com/photo/2019/02/17/17/11/karate-4002687_1280.png",
       address: "17th Middle Park, SE9 5HS London",
       contactEmail: "karate@test.com",
       isFavorite: false
     }
   ];
 
-  const addMeetup = () => {
+  let editMode;
+
+  const addMeetup = event => {
     const newMeetup = {
       id: Math.random().toString(),
-      title: title,
-      subtitle: subtitle,
-      description: description,
-      imageUrl: imageUrl,
-      contactEmail: email,
-      address: address
+      title: event.detail.title,
+      subtitle: event.detail.subtitle,
+      description: event.detail.description,
+      imageUrl: event.detail.imageUrl,
+      contactEmail: event.detail.email,
+      address: event.detail.address
     };
     // console.log(newMeetup);
 
     meetups = [newMeetup, ...meetups];
+
+    editMode = null;
   };
 
   const toggleFavorite = event => {
@@ -69,50 +66,19 @@
     margin-top: 5rem;
   }
 
-  form {
-    width: 30rem;
-    max-width: 90%;
-    margin: auto;
+  .button-new-meetup {
+    margin: 1rem;
   }
 </style>
 
 <Header />
 
 <main>
-  <form on:submit|preventDefault={addMeetup}>
-    <TextInput
-      id="title"
-      label="Title"
-      value={title}
-      on:input={event => (title = event.target.value)} />
-    <TextInput
-      id="subtitle"
-      label="Subtitle"
-      value={subtitle}
-      on:input={event => (subtitle = event.target.value)} />
-    <TextInput
-      id="address"
-      label="Address"
-      value={address}
-      on:input={event => (address = event.target.value)} />
-    <TextInput
-      id="imageUrl"
-      label="Image URL"
-      value={imageUrl}
-      on:input={event => (imageUrl = event.target.value)} />
-    <TextInput
-      id="email"
-      label="E-Mail"
-      type="email"
-      value={email}
-      on:input={event => (email = event.target.value)} />
-    <TextInput
-      id="description"
-      label="Description"
-      controlType="textarea"
-      value={description}
-      on:input={event => (description = event.target.value)} />
-    <Button type="submit" caption="Save" />
-  </form>
+  <div class="button-new-meetup">
+    <Button caption="New Meetup" on:click={() => (editMode = 'add')} />
+  </div>
+  {#if editMode === 'add'}
+    <EditMeetup on:save={addMeetup} />
+  {/if}
   <MeetupGrid {meetups} on:togglefavorite={toggleFavorite} />
 </main>
